f3cbfaade5c328ba603c86748e002f3e0d82ed3a
unknown <s14120034@alumnos.itsur.edu.mx>
2018-02-28 18:24:29 -0600

    se realizo un ejemplo con la libreria volley en la cual se obtiene una imagen de la pagina de lorempixel

---
+++ miappconvolley/AgendaContactos/app/src/main/java/com/example/gerardo/agendacontactos/actividadimagen.java
@@ -0,0 +1,13 @@
+package com.example.gerardo.agendacontactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actividadimagen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividadimagen);
+    }
+}
